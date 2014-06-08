91102774e8145a6204d02d1f0428f3c7527d2c85
Manolo Vera <manolo.vn@gmail.com>
2014-06-08 17:46:33 +0200

    added temp converter sample

---
+++ src/main/java/com/manolovn/android/soapy/sample/tempconvert/MyActivity.java
@@ -0,0 +1,57 @@
+package com.manolovn.android.soapy.sample.tempconvert;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.manolovn.android.soapy.R;
+import com.manolovn.android.soapy.Soapy;
+import com.manolovn.android.soapy.sample.lab.Lab;
+import com.manolovn.android.soapy.sample.lab.LabsApi;
+
+public class MyActivity extends Activity {
+
+    TempConvert tempConvert;
+    LabsApi labsApi;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Soapy api = new Soapy.Builder()
+                .setEndpoint("http://www.w3schools.com/webservices/tempconvert.asmx")
+                .setNamespace("http://www.w3schools.com/webservices")
+                .build();
+        tempConvert = api.create(TempConvert.class);
+
+        Soapy api2 = new Soapy.Builder()
+                .setEndpoint("http://ulabs.science.ubc.ca/pub/interface/ws_server.php")
+                .build();
+        labsApi = api2.create(LabsApi.class);
+
+        new SoapTask().execute("");
+    }
+
+    private class SoapTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String resultsString = "";
+//            resultsString = tempConvert.celsiusToFahrenheit("30");
+            Lab lab = null;
+            lab = labsApi.getLab(1);
+
+            return lab.getTitle();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Toast.makeText(MyActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
