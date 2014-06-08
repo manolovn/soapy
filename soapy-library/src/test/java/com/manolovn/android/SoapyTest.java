b11cdc1bbdca4e1fc8a759cbee4649c58b784910
Manolo Vera <manolo.vn@gmail.com>
2014-06-08 19:35:28 +0200

    maven structure

---
+++ soapy-library/src/test/java/com/manolovn/android/SoapyTest.java
@@ -0,0 +1,19 @@
+package com.manolovn.android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Soapy test
+ *
+ * @author manolovn
+ */
+public class SoapyTest {
+
+    @Test
+    public void myTest() {
+        assertEquals(1,10);
+    }
+
+}
