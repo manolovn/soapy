7c8e2dd2d273565263c883989bac66a0f0402cad
Manolo Vera <manolo.vn@gmail.com>
2014-06-09 22:36:23 +0200

    updated tests

--- soapy-library/src/test/java/com/manolovn/android/soapy/SoapyTest.java
+++ soapy-library/src/test/java/com/manolovn/android/soapy/SoapyTest.java
@@ -4,6 +4,9 @@ import com.manolovn.android.soapy.api.Lab;
 import com.manolovn.android.soapy.api.LabsApi;
 import com.manolovn.android.soapy.api.TempConvert;
 import org.junit.Test;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 
 import static org.junit.Assert.*;
 
@@ -39,6 +42,44 @@ public class SoapyTest {
         Soapy api = new Soapy.Builder()
                 .setEndpoint("http://www.w3schools.com/webservices/tempconvert.asmx")
                 .setNamespace("http://www.w3schools.com/webservices")
+                .build();
+        tempConvert = api.create(TempConvert.class);
+
+        String result = tempConvert.celsiusToFahrenheit("30");
+
+        assertEquals("86", result);
+        assertNotSame(86, result);
+    }
+
+    @Test
+    public void primitiveWithEnvelopeTest() {
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.dotNet = true;
+
+        TempConvert tempConvert;
+        Soapy api = new Soapy.Builder()
+                .setEndpoint("http://www.w3schools.com/webservices/tempconvert.asmx")
+                .setNamespace("http://www.w3schools.com/webservices")
+                .setEnvelope(envelope)
+                .build();
+        tempConvert = api.create(TempConvert.class);
+
+        String result = tempConvert.celsiusToFahrenheit("30");
+
+        assertEquals("86", result);
+        assertNotSame(86, result);
+    }
+
+    @Test
+    public void primitiveWithTransportTest() {
+        String endpoint = "http://www.w3schools.com/webservices/tempconvert.asmx";
+        HttpTransportSE httpTransportSE = new HttpTransportSE(java.net.Proxy.NO_PROXY, endpoint, 60000);
+
+        TempConvert tempConvert;
+        Soapy api = new Soapy.Builder()
+                .setEndpoint(endpoint)
+                .setNamespace("http://www.w3schools.com/webservices")
+                .setHttpTransportSE(httpTransportSE)
                 .build();
         tempConvert = api.create(TempConvert.class);
 
